97442dbff2f758ef7da11ea04d3ece96680003d9
Dakir abdelati <dakir.abdelati@gmail.com>
2019-12-11 23:41:52 +0100

    adding style to the Home

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -3,18 +3,109 @@ import styled from 'styled-components';
 
 function Home(props) {
     return (
-        <HomeDiv className="container">
-            <div className="row justify-content-center" >
-                <h1>Welcome to Projects management.</h1>
+        <HomeSection data-stellar-background-ratio="0.5">
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="row">
+                        <div className="offset col-md-6 col-sm-12">
+                            <div className="home-info">
+                                <h3>professional landing page</h3>
+                                <h1>We help you manage your website successfully!</h1>
+                                <button className="button-control">Get started</button>
+                            </div>
+                        </div>
+
+                </div>
             </div>
-        </HomeDiv>
+        </HomeSection>
     );
 }
 
-const HomeDiv = styled.div`
-    height: 30rem;
+const HomeSection = styled.section`
+    background: url('assets/images/StockSnap.jpg') no-repeat center center;
+    background-size: cover;
+    vertical-align: middle;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    min-height: 100vh;
+    position: relative;
+    padding-top: 12em;
+    text-align: center;
     h1 {
-        margin-top: 12rem;
+        color: #ffffff;
+        margin-top: 10px;
+        margin-bottom: 40px;
+    }
+    .overlay {
+        background: #536976;
+        background: -webkit-linear-gradient(to right, #292E49, #536976);
+        background: linear-gradient(to right, #292E49, #536976);
+        opacity: 0.9;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    .offset {
+        margin-left: 25%;
+        .home-info {
+            margin-top: 7em;
+            h3 {
+                color: #f0f0f0;
+                font-size: 11px;
+                font-weight: bold;
+                letter-spacing: 4px;
+                text-transform: uppercase;
+                margin: 10px 0;
+            }
+            .button-control {
+                border-color: #f0f0f0;
+                display: inline-block;
+                vertical-align: top;
+                border-radius: 50px;
+                box-shadow: none;
+                height: 50px;
+                margin-right: 0.5em;
+                padding-left: 15px;
+            }
+            button {
+                background: #29ca8e;
+                border: 0;
+                color: #ffffff;
+                right: 6em;
+                width: 25%;
+            }
+            button:hover {
+                background: #202020;
+                color: #ffffff;
+            }
+            @media screen and (max-width: 480px) {
+
+                .button-control {
+                    width: 85%;
+                    display: block;
+                    margin: 20px auto;
+                }
+                button {
+                    position: relative;
+                    right: 0;
+                }
+            }
+        }
+    }
+    @media only screen and (max-width: 992px) {
+        section {
+          padding: 60px 0;
+        }
     }
 `;
 
